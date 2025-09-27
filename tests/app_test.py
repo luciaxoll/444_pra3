6122ec1e0024c73d7b0851c23815c6359189693a
luciaxoll <luciaxol@mail.utoronto.ca>
2025-09-26 22:54:49 -0400

    Content 1 to 9

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,12 +1,10 @@
+import os
+import pytest
 import json
 from pathlib import Path
 
-import pytest
-
 from project.app import app, db
-
 TEST_DB = "test.db"
-
 
 @pytest.fixture
 def client():
@@ -76,13 +74,8 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
-
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get("/delete/1")
-    data = json.loads(rv.data)
-    assert data["status"] == 0
-    login(client, app.config["USERNAME"], app.config["PASSWORD"])
-    rv = client.get("/delete/1")
+    rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
